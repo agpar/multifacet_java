24991149a187954e0c2e8579718690a590c643be
aparmentier <a.g.parmentier@gmail.com>
2019-09-03 13:48:06 -0400

    Fixed: Will output scores while clustering.

--- src/main/python/iter_cluster.py
+++ src/main/python/iter_cluster.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
+from sklearn.metrics import silhouette_score
 
-ITERS = 0
+ITERS = 10
 
 
 def iter_cluster_greedy(dists: np.array, clust_size):
@@ -12,20 +13,30 @@ def iter_cluster_avg(dists: np.array, clust_size):
     return modified_knn_cluster(dists, clust_size, closest_points_average)
 
 
+def eval(dists, cluster_labels):
+    return silhouette_score(dists, cluster_labels, metric='precomputed')
+
+
+def cluster_labels(dists, clusters):
+    labels = np.full(len(dists), -1)
+    for key, vals in clusters.items():
+        for val in vals:
+            labels[val] = key
+    return labels
+
+
 def modified_knn_cluster(dists: np.array, clust_size, cluster_builder):
     # First cluster step
     means = sorted_with_idx(mean_dists(dists))
     clusters = refine_clusters(dists, means, clust_size, cluster_builder)
+    print(f"Initial score: {eval(dists, cluster_labels(dists, clusters))}")
 
     for i in range(ITERS):
         clusters = refine_clusters(dists, means, clust_size, cluster_builder, clusters)
+        print(f"Iter {i} score: {eval(dists, cluster_labels(dists, clusters))}")
 
     # Output cluster
-    cluster_labels = np.full(len(dists), -1)
-    for key, vals in clusters.items():
-        for val in vals:
-            cluster_labels[val] = key
-    return cluster_labels
+    return cluster_labels(dists, clusters)
 
 
 def refine_clusters(dists, means, clust_size, cluster_builder, old_clusters=None):
