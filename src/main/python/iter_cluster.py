ec4e123c236df5ff0fd5d686ce34cd622e9e675b
aparmentier <a.g.parmentier@gmail.com>
2019-09-03 13:26:26 -0400

    WIP: A better cluster.

--- src/main/python/iter_cluster.py
+++ src/main/python/iter_cluster.py
@@ -1,33 +1,67 @@
 import numpy as np
+import random
+
+ITERS = 0
 
 
 def iter_cluster_greedy(dists: np.array, clust_size):
-    return iter_cluster(dists, clust_size, closest_points_to_center)
+    return modified_knn_cluster(dists, clust_size, closest_points_to_center)
 
 
 def iter_cluster_avg(dists: np.array, clust_size):
-    return iter_cluster(dists, clust_size, closest_points_average)
+    return modified_knn_cluster(dists, clust_size, closest_points_average)
 
 
-def iter_cluster(dists: np.array, clust_size, cluster_builder):
-    already_clustered = set()
-    clusters = {}
-    cluster_num = 0
+def modified_knn_cluster(dists: np.array, clust_size, cluster_builder):
+    # First cluster step
     means = sorted_with_idx(mean_dists(dists))
-    while len(dists) - len(already_clustered) > clust_size:
-        next_center_idx = next_idx_to_cluster(means, already_clustered)
-        cluster = cluster_builder(next_center_idx, dists, clust_size, already_clustered)
-        already_clustered = already_clustered.union(cluster)
-        clusters[cluster_num] = cluster
-        cluster_num += 1
+    clusters = refine_clusters(dists, means, clust_size, cluster_builder)
 
-    last_cluster = set([idx for idx, _ in enumerate(dists) if idx not in already_clustered])
-    clusters[cluster_num] = last_cluster
+    for i in range(ITERS):
+        clusters = refine_clusters(dists, means, clust_size, cluster_builder, clusters)
+
+    # Output cluster
     cluster_labels = np.full(len(dists), -1)
     for key, vals in clusters.items():
         for val in vals:
             cluster_labels[val] = key
     return cluster_labels
+
+
+def refine_clusters(dists, means, clust_size, cluster_builder, old_clusters=None):
+    already_clustered = set()
+    clusters = {}
+    cluster_num = 0
+    if old_clusters:
+        for old_cluster in random.shuffle(old_clusters.values()):
+            next_center_idx = most_central_idx(means, dists, already_clustered, old_cluster)
+            cluster = cluster_builder(next_center_idx, dists, clust_size, already_clustered)
+            already_clustered = already_clustered.union(cluster)
+            clusters[cluster_num] = cluster
+            cluster_num += 1
+    else:
+        while len(dists) - len(already_clustered) > clust_size:
+            next_center_idx = most_central_idx(means, dists, already_clustered)
+            cluster = cluster_builder(next_center_idx, dists, clust_size, already_clustered)
+            already_clustered = already_clustered.union(cluster)
+            clusters[cluster_num] = cluster
+            cluster_num += 1
+
+    last_cluster = set([idx for idx, _ in enumerate(dists) if idx not in already_clustered])
+    clusters[cluster_num] = last_cluster
+    return clusters
+
+
+def most_central_idx(sorted_means, dists, already_clustered, relative_to=None):
+    if relative_to is None:
+        return next_idx_to_cluster(sorted_means, already_clustered)
+
+    idx_list = sorted(list(c for c in relative_to if c not in already_clustered))
+    cluster_dists = [d[idx_list] for d in dists[idx_list]]
+    means = [np.mean(clu_dist) for clu_dist in cluster_dists]
+    min_mean = min(means)
+    min_mean_idx = means.index(min_mean)
+    return idx_list[min_mean_idx]
 
 
 def closest_points_to_center(center_idx, dists, n, already_clustered):
