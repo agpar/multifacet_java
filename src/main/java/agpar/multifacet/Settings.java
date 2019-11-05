d423061600104c14a08520f266aeef1c0378f1cf
aparmentier <a.g.parmentier@gmail.com>
2019-11-05 15:14:38 -0500

    Fixed: Included settings.java changes.
    These should have been included in
    1d970547da6fad2a0f43eb372e90cc35ea033788

--- src/main/java/agpar/multifacet/Settings.java
+++ src/main/java/agpar/multifacet/Settings.java
@@ -1,10 +1,75 @@
 package agpar.multifacet;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Properties;
 
 public class Settings {
-    public static String DATA_DIR =  "/home/aparment/Documents/datasets/yelp";
-    public static String RAM_DATA_DIR = "/home/aparment/Documents/datasets/yelp/in_memory";
 
-    public static String EXPERIMENT_DIR = "/home/aparment/Documents/multifacet_experiments";
-    public static String PYTHON_PROJECT_DIR = "/home/aparment/Documents/multifacet_java/src/main/python";
+    private String RAM_DATA_DIR;
+    private String EXPERIMENT_DIR;
+    private String PYTHON_PROJECT_DIR;
+
+    private static Settings settings;
+
+    private Settings() {
+        String settings_path = settingFilePath();
+        String multifacet_root = multifacetRoot();
+        Properties prop = new Properties();
+        try {
+            prop.load(new FileReader(settings_path));
+            this.EXPERIMENT_DIR = prop.getProperty("experiment_dir");
+            this.RAM_DATA_DIR = prop.getProperty("ram_data_dir");
+            this.PYTHON_PROJECT_DIR = Path.of(multifacet_root, "src/main/python").toString();
+        } catch (FileNotFoundException e) {
+            System.out.printf("ERROR: Could not find settings file at %s\n", settings_path);
+            System.exit(1);
+        } catch (IOException e) {
+            System.out.println("ERROR: Failed to close settings file.");
+            System.exit(1);
+        }
+    }
+
+    private static String settingFilePath() {
+        String multifacet_root = multifacetRoot();
+        String settings_default_path = Path.of(multifacet_root, "settings.properties").toString();
+        String settings_local_path = Path.of(multifacet_root, "settings_local.properties").toString();
+        if (new File(settings_local_path).exists()) {
+            return settings_local_path;
+        }
+        return settings_default_path;
+    }
+
+    private static String multifacetRoot() {
+        String multifacet_root = System.getenv("MULTIFACET_ROOT");
+        if (multifacet_root == null) {
+            System.out.println("ERROR: MULTIFACET_ROOT is not defined.");
+            System.exit(1);
+        }
+        return multifacet_root;
+    }
+
+    private static void init() {
+        if (settings == null) {
+            settings = new Settings();
+        }
+    }
+
+    public static String RAM_DATA_DIR() {
+        init();
+        return settings.RAM_DATA_DIR;
+    }
+
+    public static String EXPERIMENT_DIR() {
+        init();
+        return settings.EXPERIMENT_DIR;
+    }
+
+    public static String PYTHON_PROJECT_DIR() {
+        init();
+        return settings.PYTHON_PROJECT_DIR;
+    }
 }
