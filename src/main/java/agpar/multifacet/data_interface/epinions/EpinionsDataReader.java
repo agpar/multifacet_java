6fe7bfa9d9a332d884dace32cd926fbd59e20c12
aparmentier <a.g.parmentier@gmail.com>
2019-11-25 14:03:09 -0500

    Fixed: Syntax error.

--- src/main/java/agpar/multifacet/data_interface/epinions/EpinionsDataReader.java
+++ src/main/java/agpar/multifacet/data_interface/epinions/EpinionsDataReader.java
@@ -80,7 +80,7 @@ public class EpinionsDataReader {
                     users.put(new User(
                             "null",
                             userId,
-                            friends.getOrDefault(userId, new HashSet<Integer>)
+                            friends.getOrDefault(userId, new HashSet<Integer>())
                     ));
                 }
                 line = reader.readLine();
