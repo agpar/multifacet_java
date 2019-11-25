4fd1a47cacc46c68b1743be1af7287a786962feb
aparmentier <a.g.parmentier@gmail.com>
2019-11-25 11:17:49 -0500

    New: Result calculator with a good relevance check.
    This calculator does not output pairs for users who have no
    social or common item reviewed overlap, implying that there is is
    highly unlikely that the users know eachother.

---
+++ src/main/java/agpar/multifacet/pairwise/result_calculators/RelevantConnectionsCalculator.java
@@ -0,0 +1,40 @@
+package agpar.multifacet.pairwise.result_calculators;
+
+import agpar.multifacet.data_interface.yelp.data_classes.User;
+import agpar.multifacet.pairwise.PairwiseMetrics;
+import agpar.multifacet.pairwise.PairwiseResult;
+import agpar.multifacet.pairwise.review_avg_calculators.ReviewAvgCalculator;
+
+/*
+Only returns a result when the pair of users have either a item-reviewed or social overlap.
+The idea being that if they have no friends in common and have never reviewed a common item,
+then there is essentially no basis to expect these users to know each other.
+ */
+public class RelevantConnectionsCalculator extends ResultCalculator {
+
+    public RelevantConnectionsCalculator(ReviewAvgCalculator avgCalculator, int minPCCOverlap) {
+        super(avgCalculator, minPCCOverlap);
+    }
+
+    @Override
+    public PairwiseResult calc(User user1, User user2) {
+        double socialJacc = PairwiseMetrics.socialJaccard(user1, user2);
+        double itemJacc = PairwiseMetrics.itemJaccard(user1, user2);
+        if (socialJacc == 0 && itemJacc == 0) {
+            return null;
+        }
+        Double pcc = PairwiseMetrics.reviewPcc(user1, user2, this.avgCalculator, this.minPCCOverlap);
+        boolean areFriends = PairwiseMetrics.areFriends(user1, user2);
+        double categoryJacc = PairwiseMetrics.categoryJaccard(user1, user2);
+        return new PairwiseResult(
+                user1.getUserIdInt(),
+                user2.getUserIdInt(),
+                pcc,
+                socialJacc,
+                areFriends,
+                socialJacc > 0,
+                itemJacc,
+                categoryJacc
+        );
+    }
+}
