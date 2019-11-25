a714523d8f1f11896376833089cea072039ec322
aparmentier <a.g.parmentier@gmail.com>
2019-11-25 13:48:17 -0500

    Fixed: Finished the epinions data set.
    This is essentially identical to the Yelp one now, but I will
    keep is distinct assuming some other distinctions may arise.

--- src/main/java/agpar/multifacet/data_interface/epinions/EpinionsData.java
+++ src/main/java/agpar/multifacet/data_interface/epinions/EpinionsData.java
@@ -2,6 +2,12 @@ package agpar.multifacet.data_interface.epinions;
 
 import agpar.multifacet.Settings;
 import agpar.multifacet.data_interface.DataSet;
+import agpar.multifacet.data_interface.collections.ReviewsById;
+import agpar.multifacet.data_interface.data_classes.Business;
+import agpar.multifacet.data_interface.data_classes.Review;
+import agpar.multifacet.data_interface.data_classes.User;
+
+import java.util.List;
 
 public class EpinionsData extends DataSet {
     private EpinionsDataReader reader;
@@ -18,5 +24,25 @@ public class EpinionsData extends DataSet {
         this.reviewsByItemId = reader.loadTrainReviews();
         System.out.println("Loading Businesses");
         this.businesses = reader.loadBusinesses();
+
+        // Adding reviews and categories to users.
+        ReviewsById reviewsByUserId = new ReviewsById();
+        for (List<Review> reviews: this.reviewsByItemId.values()) {
+            for (Review review: reviews) {
+                reviewsByUserId.put(review.getUserIdInt(), review);
+            }
+        }
+
+        for (User user : this.usersById.values()) {
+            user.addReviews(reviewsByUserId.get(user.getUserIdInt()));
+        }
+
+        // Adding categories
+        for(User user: this.usersById.values()) {
+            for (Review review : user.getReviews()) {
+                Business business = this.businesses.get(review.getItemIdInt());
+                user.getCategoriesReviewed().addAll(business.categories);
+            }
+        }
     }
 }
