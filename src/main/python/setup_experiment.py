5a4082918fefc0dd51607aac283ba2e013decf8a
aparment <a.g.parmentier@gmail.com>
2019-12-06 14:46:58 -0500

    New: Added option to skip to a step.

--- src/main/python/setup_experiment.py
+++ src/main/python/setup_experiment.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 """Generates pairwise and single, clusters and predictions for an experiment."""
 import argparse
 import json
@@ -84,7 +86,13 @@ def generate_predictions(experiment_dir, single_path, pairwise_path, pcc_cluster
         p.join()
 
 
-def run(experiment_dir=None, data_set=None):
+STEPS = ("filter", "single", "pairwise", "dists", "cluster", "predict")
+def run(experiment_dir=None, data_set=None, skipto=None):
+    if skipto is None:
+        skip_to = STEPS.index("filter")
+    else:
+        skip_to = STEPS.index(skipto)
+
     data_set = DataSetEnum(data_set)
     if data_set.IS_EPINIONS:
         print(f"Setting up all data files for EPINIONS in {experiment_dir}")
@@ -94,51 +102,59 @@ def run(experiment_dir=None, data_set=None):
     if not os.path.exists(experiment_dir):
         os.makedirs(experiment_dir, exist_ok=True)
 
-    print("Filtering data...")
-    filter_data(data_set)
+    if STEPS.index("filter") >= skip_to:
+        print("Filtering data...")
+        filter_data(data_set)
 
-    print("Generating single user data...")
     single_path = os.path.join(experiment_dir, "single_feats.csv")
-    generate_single(data_set, single_path)
+    if STEPS.index("single") >= skip_to:
+        print("Generating single user data...")
+        generate_single(data_set, single_path)
 
-    print("Generating pairwise...")
     pairwise_path = os.path.join(experiment_dir, "pairwise_feats.csv")
-    generate_pairwise(data_set, pairwise_path)
+    if STEPS.index("pairwise") >= skip_to:
+        print("Generating pairwise...")
+        generate_pairwise(data_set, pairwise_path)
 
-    print("Saving dist matrices...")
     pcc_dist_matrix_path = os.path.join(experiment_dir, "pcc_dists.npy")
-    pcc_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "pcc")
-    np.save(pcc_dist_matrix_path, pcc_dist_matrix)
-
     social_dist_matrix_path = os.path.join(experiment_dir, "social_dists.npy")
-    social_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "social")
-    np.save(social_dist_matrix_path, social_dist_matrix)
+    if STEPS.index("dists") >= skip_to:
+        print("Saving dist matrices...")
+        pcc_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "pcc")
+        np.save(pcc_dist_matrix_path, pcc_dist_matrix)
 
-    print("Determining optimal cluster count...")
-    k_goodness_pcc = choose_k(clusteroid_kmeans.cluster, pcc_dist_matrix_path, range(15, 60), 20, eval_silouette)
-    with open(os.path.join(experiment_dir, "k_pcc_results.json"), 'w') as f:
-        json.dump(k_goodness_pcc, f)
+        social_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "social")
+        np.save(social_dist_matrix_path, social_dist_matrix)
 
-    k_goodness_social = choose_k(clusteroid_kmeans.cluster, social_dist_matrix_path, range(15, 60), 20, eval_silouette)
-    with open(os.path.join(experiment_dir, "k_social_results.json"), 'w') as f:
-        json.dump(k_goodness_social, f)
-
-    print("Generating clusters...")
     pcc_cluster_path = os.path.join(experiment_dir, "pcc_clusters.json")
-    cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, k_goodness_pcc[0][0], 50)
-    #cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, 50, 50)
-
     social_cluster_path = os.path.join(experiment_dir, "social_clusters.json")
-    cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, k_goodness_social[0][0], 50)
-    #cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, 50, 50)
+    if STEPS.index("cluster") >= skip_to:
+        print("Determining optimal cluster count...")
+        k_goodness_pcc = choose_k(clusteroid_kmeans.cluster, pcc_dist_matrix_path, range(15, 60), 20, eval_silouette)
+        with open(os.path.join(experiment_dir, "k_pcc_results.json"), 'w') as f:
+            json.dump(k_goodness_pcc, f)
 
-    print("Running all predictions in parallel.")
-    generate_predictions(experiment_dir, single_path, pairwise_path, pcc_cluster_path, social_cluster_path)
+        k_goodness_social = choose_k(clusteroid_kmeans.cluster, social_dist_matrix_path, range(15, 60), 20, eval_silouette)
+        with open(os.path.join(experiment_dir, "k_social_results.json"), 'w') as f:
+            json.dump(k_goodness_social, f)
+
+        print("Generating clusters...")
+        cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, k_goodness_pcc[0][0], 50)
+        #cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, 50, 50)
+
+        cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, k_goodness_social[0][0], 50)
+        #cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, 50, 50)
+
+    if STEPS.index("predict") >= skip_to:
+        print("Running all predictions in parallel.")
+        generate_predictions(experiment_dir, single_path, pairwise_path, pcc_cluster_path, social_cluster_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform all setup necessary to run a new set of prediction experiments.')
     parser.add_argument('experiment_dir',  type=str, help='The directory to store all the goodies.')
     parser.add_argument('data_set', type=str, choices={'yelp', 'epinions'}, help='Which data set to use.')
+    parser.add_argument('--skipto', type=str, choices=STEPS, default=STEPS[0],
+                        help='Skip to a certain step, assuming output of previous steps is already available.')
     args = parser.parse_args()
     run(**vars(args))
