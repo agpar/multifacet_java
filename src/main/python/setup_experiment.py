6910df1f8864b1603d0963ff1bcc94993961cea6
aparment <a.g.parmentier@gmail.com>
2020-01-09 15:50:20 -0500

    Fixed: setup_experiment generates rating tuples.

--- src/main/python/setup_experiment.py
+++ src/main/python/setup_experiment.py
@@ -21,6 +21,8 @@ if "MULTIFACET_ROOT" not in os.environ:
     raise Exception("$MULTIFACET_ROOT is not set. Can't determine where project is located.")
 MULTIFACET_ROOT = os.environ["MULTIFACET_ROOT"]
 
+SETUP_STEPS = ("filter", "single", "pairwise", "dists", "cluster", "predict", "tuples")
+
 
 class DataSetEnum:
     def __init__(self, data_str):
@@ -86,18 +88,19 @@ def generate_predictions(experiment_dir, single_path, pairwise_path, pcc_cluster
         processes.append(p)
     for p in processes:
         p.join()
-    #
-    #    for task in prediction_tasks:
-    #        if os.path.exists(task[0]):
-    #            shutil.move(task[0], task[0] + ".old")
-    #        predict.run(*(base_args + task))
 
-STEPS = ("filter", "single", "pairwise", "dists", "cluster", "predict")
+
+def generate_rating_tuples(data_set, output_path):
+    executable = os.path.join(MULTIFACET_ROOT, "run.sh")
+    data_flag = "--epinions" if data_set.IS_EPINIONS else ""
+    os.system(f"{executable} --genTuples {data_flag} {output_path}")
+
+
 def run(experiment_dir=None, data_set=None, skipto=None):
     if skipto is None:
-        skip_to = STEPS.index("filter")
+        skip_to = SETUP_STEPS.index("filter")
     else:
-        skip_to = STEPS.index(skipto)
+        skip_to = SETUP_STEPS.index(skipto)
 
     data_set = DataSetEnum(data_set)
     if data_set.IS_EPINIONS:
@@ -108,23 +111,23 @@ def run(experiment_dir=None, data_set=None, skipto=None):
     if not os.path.exists(experiment_dir):
         os.makedirs(experiment_dir, exist_ok=True)
 
-    if STEPS.index("filter") >= skip_to:
+    if SETUP_STEPS.index("filter") >= skip_to:
         print("Filtering data...")
         filter_data(data_set)
 
     single_path = os.path.join(experiment_dir, "single_feats.csv")
-    if STEPS.index("single") >= skip_to:
+    if SETUP_STEPS.index("single") >= skip_to:
         print("Generating single user data...")
         generate_single(data_set, single_path)
 
     pairwise_path = os.path.join(experiment_dir, "pairwise_feats.csv")
-    if STEPS.index("pairwise") >= skip_to:
+    if SETUP_STEPS.index("pairwise") >= skip_to:
         print("Generating pairwise...")
         generate_pairwise(data_set, pairwise_path)
 
     pcc_dist_matrix_path = os.path.join(experiment_dir, "pcc_dists.npy")
     social_dist_matrix_path = os.path.join(experiment_dir, "social_dists.npy")
-    if STEPS.index("dists") >= skip_to:
+    if SETUP_STEPS.index("dists") >= skip_to:
         print("Saving dist matrices...")
         pcc_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "pcc")
         np.save(pcc_dist_matrix_path, pcc_dist_matrix)
@@ -132,9 +135,10 @@ def run(experiment_dir=None, data_set=None, skipto=None):
         social_dist_matrix = cluster.gen_dist_matrix(single_path, pairwise_path, "social")
         np.save(social_dist_matrix_path, social_dist_matrix)
 
+    # TODO figure out why this step sometimes hangs.
     pcc_cluster_path = os.path.join(experiment_dir, "pcc_clusters.json")
     social_cluster_path = os.path.join(experiment_dir, "social_clusters.json")
-    if STEPS.index("cluster") >= skip_to:
+    if SETUP_STEPS.index("cluster") >= skip_to:
         print("Determining optimal cluster count...")
         k_goodness_pcc = choose_k(clusteroid_kmeans.cluster, pcc_dist_matrix_path, range(15, 60), 20, eval_silouette)
         with open(os.path.join(experiment_dir, "k_pcc_results.json"), 'w') as f:
@@ -146,23 +150,21 @@ def run(experiment_dir=None, data_set=None, skipto=None):
 
         print("Generating clusters...")
         cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, k_goodness_pcc[0][0], 50)
-        #cluster.run(single_path, pairwise_path, "pcc", pcc_cluster_path, [pcc_dist_matrix_path], None, 50, 50)
-
         cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, k_goodness_social[0][0], 50)
-        #cluster.run(single_path, pairwise_path, "social", social_cluster_path, [social_dist_matrix_path], None, 50, 50)
 
-    #TODO add rating tuple generation.
-
-    if STEPS.index("predict") >= skip_to:
+    if SETUP_STEPS.index("predict") >= skip_to:
         print("Running all predictions in parallel.")
         generate_predictions(experiment_dir, single_path, pairwise_path, pcc_cluster_path, social_cluster_path)
+
+    rating_tuple_path = os.path.join(experiment_dir, "rating_tuples.txt")
+    generate_rating_tuples(data_set, rating_tuple_path)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Perform all setup necessary to run a new set of prediction experiments.')
     parser.add_argument('experiment_dir',  type=str, help='The directory to store all the goodies.')
     parser.add_argument('data_set', type=str, choices={'yelp', 'epinions'}, help='Which data set to use.')
-    parser.add_argument('--skipto', type=str, choices=STEPS, default=STEPS[0],
+    parser.add_argument('--skipto', type=str, choices=SETUP_STEPS, default=SETUP_STEPS[0],
                         help='Skip to a certain step, assuming output of previous steps is already available.')
     args = parser.parse_args()
     run(**vars(args))
