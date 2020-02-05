9a2c221bb3c71213449e01aca2340bc1950fe438
aparmentier <a.g.parmentier@gmail.com>
2020-02-05 13:33:48 -0500

    WIp: Stats outputs data.

--- src/main/java/agpar/multifacet/commands/GenerateStats.java
+++ src/main/java/agpar/multifacet/commands/GenerateStats.java
@@ -4,6 +4,15 @@ import agpar.multifacet.data_interface.DATA_SOURCE;
 import agpar.multifacet.data_interface.DataSet;
 import agpar.multifacet.stats.DataStatsCalculator;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class GenerateStats implements Command {
     private DATA_SOURCE source;
 
@@ -18,19 +27,26 @@ public class GenerateStats implements Command {
 
         System.out.printf("Generated stats for %s dataset.\n", source.toString());
 
-        System.out.print("User Review Counts\n");
-        System.out.printf("%s\n", DataStatsCalculator.userReviewCount(ds).toString());
+        printAndWrite("User Review Counts", DataStatsCalculator.userReviewCount(ds));
+        printAndWrite("User Average Review Scores", DataStatsCalculator.userReviewAverages(ds));
+        printAndWrite("UserFriendCount", DataStatsCalculator.friendsPerUser(ds));
+        printAndWrite("Item Review Counts", DataStatsCalculator.itemReviewCount(ds));
+        printAndWrite("Global Review Scores", DataStatsCalculator.reviewScores(ds));
+    }
 
-        System.out.print("User Average Review Scores\n");
-        System.out.printf("%s\n", DataStatsCalculator.userReviewAverages(ds).toString());
+    private void printAndWrite(String title, DataStatsCalculator.StatsBundle stats) {
+        String outputFile = "data_" + title.toLowerCase().replace(" ", "_") + ".txt";
 
-        System.out.print("User Friend Counts\n");
-        System.out.printf("%s\n", DataStatsCalculator.friendsPerUser(ds).toString());
+        System.out.println(title);
+        System.out.printf("%s\n", stats.toString());
 
-        System.out.print("Item Review Counts\n");
-        System.out.printf("%s\n", DataStatsCalculator.itemReviewCount(ds).toString());
-
-        System.out.print("Global Review scores\n");
-        System.out.printf("%s\n", DataStatsCalculator.reviewScores(ds).toString());
+        try {
+            Writer writer = new BufferedWriter(new FileWriter(outputFile));
+            List<String> vals = Stream.of(stats.values).map(Object::toString).collect(Collectors.toList());
+            writer.write(String.join(",", vals));
+            writer.close();
+        } catch (IOException e) {
+            System.out.printf("Failed to output data to %s", outputFile);
+        }
     }
 }
