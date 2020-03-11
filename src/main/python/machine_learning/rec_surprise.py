82cd83f5d0812a8184829c2ad740f042a4f1f6e7
aparment <a.g.parmentier@gmail.com>
2020-03-11 10:44:52 -0400

    WIP: Update surprise recommender.
    Added a pool function to predict multiple things at a time, but
    the PCC calculation uses a huge multiple of necessary memory, so it
    will have to be precomputed and dumped before actually running.

--- src/main/python/machine_learning/rec_surprise.py
+++ src/main/python/machine_learning/rec_surprise.py
@@ -5,8 +5,9 @@ from scipy.sparse import coo_matrix
 from surprise import Reader, Dataset
 from surprise. model_selection import PredefinedKFold
 from surprise import accuracy
-from tqdm import tqdm
+from surprise.similarities import pearson
 import os
+from multiprocessing import Pool
 
 
 class MauroU2UCF(KNNWithMeans):
@@ -17,13 +18,11 @@ class MauroU2UCF(KNNWithMeans):
 
     def compute_similarities(self):
         sims = super().compute_similarities()
-        print("Applying trust to similarities")
-
         # Assert inner ids are equiv to outer ids.
         for i in range(sims.shape[0]):
             assert(i == int(self.trainset.to_raw_uid(i)))
 
-        for i in tqdm(range(sims.shape[0])):
+        for i in range(sims.shape[0]):
             trust_row = self.trust_mat.get_row(i)
             non_empty_indices = trust_row.indices
             adjusted_sims = []
@@ -66,7 +65,22 @@ class SparseTrustMatrix:
         return self.mat.getrow(i)
 
 
-def evaluate_links(experiment_dir, trust_file):
+def perform_precomputation(experiment_dir):
+    rating_train_path = os.path.join(experiment_dir, 'ratings_train.txt')
+    rating_test_path = os.path.join(experiment_dir, 'ratings_test.txt')
+    ratings_reader = Reader(line_format="user item rating", sep=' ')
+    dataset = Dataset.load_from_folds([(rating_train_path, rating_test_path)], ratings_reader)
+    pkf = PredefinedKFold()
+    trainset, testset = list(pkf.split(dataset))[0]
+
+    n_x, yr = trainset.n_users, trainset.ir
+    min_support = 1
+    args = [n_x, yr, min_support]
+    sims = pearson(*args).astype(np.float32)
+    return trainset, testset, sims
+
+
+def evaluate_links(experiment_dir, trust_file, **kwargs):
     trust_path = os.path.join(experiment_dir, trust_file)
     trust_mat = SparseTrustMatrix(trust_path)
 
@@ -77,7 +91,10 @@ def evaluate_links(experiment_dir, trust_file):
     pkf = PredefinedKFold()
     trainset, testset = list(pkf.split(dataset))[0]
 
-    algo = MauroU2UCF(beta=0.1, trust_mat=trust_mat, k=50, sim_options={'name':'pearson', 'min_support':3} )
+    beta = kwargs.get('beta', 0.1)
+    k = kwargs.get('k', 50)
+
+    algo = MauroU2UCF(beta=beta, trust_mat=trust_mat, k=k, sim_options={'name':'msd', 'min_support':3} )
     fitted_algo = algo.fit(trainset)
     predictions = fitted_algo.test(testset)
     return {
@@ -85,3 +102,35 @@ def evaluate_links(experiment_dir, trust_file):
         "RMSE": accuracy.rmse(predictions),
         "predictions": predictions
     }
+
+
+def evaluate_all_links(experiment_dir, trust_files, **kwargs):
+    # Assert all files exist:
+    for trust_file in trust_files:
+        full_path = os.path.join(experiment_dir, trust_file)
+        if not os.path.exists(full_path):
+            raise Exception(f"Prediction file does not exist: {full_path}")
+
+    beta = kwargs.get('beta', 0.1)
+    k = kwargs.get('k', 50)
+    pool_args = []
+    pool_kwargs = []
+
+    for trust_file in trust_files:
+        pool_args.append((experiment_dir, trust_file))
+        pool_kwargs.append({'beta': beta, 'k': k})
+
+    pool = Pool()
+    results = []
+    for args, kwargs in zip(pool_args, pool_kwargs):
+        exp_name = "_".join(map(str, (args[1], kwargs['beta'], kwargs['k'])))
+        results.append((exp_name, pool.apply_async(evaluate_links, args=args, kwds=kwargs)))
+
+    pool.close()
+    all_results = []
+    for exp_name, res in results:
+        exp_results = res.get()
+        all_results.append((exp_name, exp_results))
+    pool.join()
+
+    return all_results
